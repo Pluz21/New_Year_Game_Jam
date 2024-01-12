c4f1850f8c765ddf9a84cbb315f0eab9f2357ead
Tim VL <vanleemput.tim@gmail.com>
2024-01-12 13:09:00 +0100

    Bug fixes
    - No longer possible to activate numbers while they are rotating

---
+++ NYNS_GameJam_Project/Source/NYNS_GameJam_Project/Actors/LockTumbler.cpp
@@ -0,0 +1,27 @@
+// Made by Skyler, Pluz21, KingCache,Annihilator & BigRizzlerK
+
+
+#include "NYNS_GameJam_Project/Actors/LockTumbler.h"
+
+// Sets default values
+ALockTumbler::ALockTumbler()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALockTumbler::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALockTumbler::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
